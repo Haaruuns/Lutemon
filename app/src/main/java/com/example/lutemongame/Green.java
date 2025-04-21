046b9b5a099d9baace6309f9b5fdd4ddc16b2b52
Dawva <dekan.ghazi@student.lut.fi>
2025-04-21 17:24:45 +0300

    korjaukset/testi, dekan approovaa

--- app/src/main/java/com/example/lutemongame/Green.java
+++ app/src/main/java/com/example/lutemongame/Green.java
@@ -3,6 +3,6 @@ package com.example.lutemongame;
 public class Green extends Lutemon{
     public Green(String name) {
         super(name, "Green", 6, 3, 19);
-        this.image = R.drawable.img_4;
+        this.image = R.drawable.green_lutemon;
     }
 }
